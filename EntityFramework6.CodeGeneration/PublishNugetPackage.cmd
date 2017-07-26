37fa717f04e9d51cdd9597b6852cb4984e57e7e1
Anastasya Kuznetsova <KuznetsovaA@quantumart.ru>
2017-07-26 16:13:56 +0300

    fix path to dll

--- EntityFramework6.CodeGeneration/PublishNugetPackage.cmd
+++ EntityFramework6.CodeGeneration/PublishNugetPackage.cmd
@@ -1,2 +1,2 @@
-﻿nuget pack NugetPackage.nuspec
-nuget push QP8.EntityFramework6.1.1.4.nupkg -Source file://mscbuild02.artq.com/Packages/
+nuget pack NugetPackage.nuspec
+nuget push QP8.EntityFramework6.1.1.5.nupkg -Source file://mscbuild02.artq.com/Packages/
