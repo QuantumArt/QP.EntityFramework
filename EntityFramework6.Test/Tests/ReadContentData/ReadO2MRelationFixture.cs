21a792a103273ca70a5d46b01ee0b917a2ae8241
Anastasya Kuznetsova <KuznetsovaA@quantumart.ru>
2017-08-24 18:42:50 +0300

    fix tests and increment package version

--- EntityFramework6.Test/Tests/ReadContentData/ReadO2MRelationFixture.cs
+++ EntityFramework6.Test/Tests/ReadContentData/ReadO2MRelationFixture.cs
@@ -11,29 +11,41 @@ namespace EntityFramework6.Test.Tests.ReadContentData
     {
         [Test]
         [Category("ReadContentData")]
-        public void Check_That_o2m_Relation_Field_IsLoaded([ContentAccessValues] ContentAccess access, [MappingValues] Mapping mapping)
+        public void Check_That_o2m_Relation_Field_toMapped_IsLoaded([ContentAccessValues] ContentAccess access, [MappingValues] Mapping mapping)
         {
             using (var context = GetDataContext(access, mapping))
             {
-                var item = context.OtMItemsForMapping.Take(5).ToArray();
+                var item = context.OtMItemsForMapping.FirstOrDefault();
                 var related = context.OtMRelatedItemsWithMapping.FirstOrDefault();
                 Assert.That(item, Is.Not.Null);
                 Assert.That(related, Is.Not.Null);
-
+                Assert.AreEqual(item.OtMReferenceMapping_ID, related.Id);
+                Assert.AreEqual(item.OtMReferenceMapping.Id, related.Id);
             }
         }
 
         [Test]
         [Category("ReadContentData")]
-        public void Check_That_o2m_Relation_Field_IsIncluded([ContentAccessValues] ContentAccess access, [MappingValues] Mapping mapping)
+        public void Check_That_o2m_Relation_Field_toNotMapped_IsLoaded([ContentAccessValues] ContentAccess access, [MappingValues] Mapping mapping)
         {
             using (var context = GetDataContext(access, mapping))
             {
-                var item = context.OtMItemsForMapping.Include("OtMReferenceMapping").Take(5).ToArray();
-               // var related = context.OtMRelatedItemsWithMapping.FirstOrDefault();
+                var item = context.OtMItemsToContentWithoutMapping.FirstOrDefault();
                 Assert.That(item, Is.Not.Null);
-                //Assert.That(related, Is.Not.Null);
-
+                Assert.That(item.OtMReferenceMapping, Is.Not.Null);
+            }
+        }
+        
+        [Test]
+        [Category("ReadContentData")]
+        public void Check_That_o2m_Relation_Field_toMapped_IsIncluded([ContentAccessValues] ContentAccess access, [MappingValues] Mapping mapping)
+        {
+            using (var context = GetDataContext(access, mapping))
+            {
+                var item = context.OtMItemsForMapping.Include("OtMReferenceMapping").FirstOrDefault();
+                Assert.That(item, Is.Not.Null);
+                Assert.That(item.OtMReferenceMapping, Is.Not.Null);
+                Assert.That(item.OtMReferenceMapping_ID, Is.Not.Null);
             }
         }
     }
