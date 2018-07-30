048700298889d1e8d4f2d192eaf77547bc25e31d
Sergey Koryakin <KoryakinS@quantumart.ru>
2018-07-30 12:38:10 +0300

    #125021 add test project for asp net core

---
+++ EntityFramework6.AspNetCore.Test/DataContext/IQPFormService.cs
@@ -0,0 +1,8 @@
+namespace EntityFramework6.AspNetCore.Test.DataContext
+{
+    public interface IQPFormService
+    {
+        string GetFormNameByNetNames(string netContentName, string netFieldName);
+        string ReplacePlaceholders(string text);
+    }
+}
---
+++ EntityFramework6.AspNetCore.Test/DataContext/StatusType.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EntityFramework6.AspNetCore.Test.DataContext
+{
+    public partial class StatusType
+    {
+        public Int32 Id { get; set; }
+        public string StatusTypeName { get; set; }
+        public int Weight { get; set; }
+        public Int32 SiteId { get; set; }
+    }
+}
