a082528049f331b44ba51bc11e46db1f4ff86c57
Pavel Celut <CelutP@quantumart.ru>
2022-09-08 18:42:48 +0300

    Template fixes

--- EntityFramework6.CodeGeneration/PublishNugetPackage.cmd
+++ EntityFramework6.CodeGeneration/PublishNugetPackage.cmd
@@ -1,2 +1,2 @@
 nuget pack NugetPackage.nuspec
-nuget push QP8.EntityFramework6.1.4.0-beta1.nupkg -Source file://mscbuild02.artq.com/Packages/
+nuget push QP8.EntityFramework6.1.4.0-beta2.nupkg -Source file://mscbuild02.artq.com/Packages/
